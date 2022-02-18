d6451d1422642c9b87e94adc17a84518e2d572a6
Arthur <r.thur.light@gmail.com>
2022-02-18 16:58:56 +0000

    Add api

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -31,7 +31,7 @@
             <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
                 <!--begin::Logo-->
                 <a href="../../demo1/dist/index.html" class="mb-12">
-                    <img alt="Logo" src="assets/media/logos/logo-1.svg" class="h-40px" />
+                    <img alt="Logo" src="/assets/media/logos/faso_logistique.png" class="h-100px" />
                 </a>
                 <!--end::Logo-->
                 <!--begin::Wrapper-->
